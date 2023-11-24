5fcd8ca53a25b90dc6fc7692124d23f6595d15fd
Nilay Parikh <me@nilayparikh.com>
2023-11-24 22:45:10 +0000

    feat(tests): more unit tests

--- 2-setup-pyenv-pipx/test_setup.sh
+++ 2-setup-pyenv-pipx/test_setup.sh
@@ -14,3 +14,42 @@ load 'setup.sh'
   run command -v yq
   [ "$status" -eq 0 ]
 }
+
+@test "install_pipx installs pip if not already installed" {
+  # Mock command -v to simulate pip not being installed
+  run command -v pip
+  [ "$status" -eq 1 ]
+
+  # Run the install_pipx function
+  run install_pipx
+
+  # Check that pip is now installed
+  run command -v pip
+  [ "$status" -eq 0 ]
+}
+
+@test "install_pipx installs pipx if not already installed" {
+  # Mock command -v to simulate pipx not being installed
+  run command -v pipx
+  [ "$status" -eq 1 ]
+
+  # Run the install_pipx function
+  run install_pipx
+
+  # Check that pipx is now installed
+  run command -v pipx
+  [ "$status" -eq 0 ]
+}
+
+@test "install_pipx upgrades pipx if already installed" {
+  # Mock command -v to simulate pipx being installed
+  run command -v pipx
+  [ "$status" -eq 0 ]
+
+  # Run the install_pipx function
+  run install_pipx
+
+  # Check that pipx is still installed
+  run command -v pipx
+  [ "$status" -eq 0 ]
+}
